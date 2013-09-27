67dda0914154e9c545424798a683bfca28d84acc
Clinton Gomes <clinton.gomes@lelesys.com>
2013-09-27 11:27:52 +0530

    [TASK] Implemented meta data plugin
    Meta data plugin
    
    Sprint: 2013-39
    Relates: #14161
    
    Change-Id: I916b87e87baaeb727bc18f901c48f0cc5b980f42

---
+++ Classes/Lelesys/Plugin/News/TypoScript/MetaDataImplementation.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Lelesys\Plugin\News\TypoScript;
+
+/* *
+ * This script belongs to the package "Lelesys.Plugin.News".                    *
+ *                                                                              *
+ * It is free software; you can redistribute it and/or modify it under          *
+ * the terms of the GNU Lesser General Public License, either version 3         *
+ * of the License, or (at your option) any later version.                       *
+ *                                                                              */
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * MetaData TypoScript object implementation
+ *
+ * @Flow\Scope("prototype")
+ */
+class MetaDataImplementation extends \TYPO3\TypoScript\TypoScriptObjects\TemplateImplementation {
+
+	/**
+	 * News service
+	 *
+	 * @Flow\Inject
+	 * @var \Lelesys\Plugin\News\Domain\Service\NewsService
+	 */
+	protected $newsService;
+
+	/**
+	 * Overide the parent evaluate method to get news object
+	 * and assign to template
+	 *
+	 * @return string
+	 */
+	public function evaluate() {
+		$fluidTemplate = new \TYPO3\TypoScript\View\FluidView(($this->tsRuntime->getControllerContext()->getRequest() instanceof \TYPO3\Flow\Mvc\ActionRequest) ? $this->tsRuntime->getControllerContext()->getRequest() : NULL);
+
+		$templatePath = $this->tsValue('templatePath');
+		if ($templatePath === NULL) {
+			throw new \Exception('Template path "' . $templatePath . '" at path "' . $this->path . '"  not found');
+		}
+		$fluidTemplate->setTemplatePathAndFilename($templatePath);
+
+		$partialRootPath = $this->tsValue('partialRootPath');
+		if ($partialRootPath !== NULL) {
+			$fluidTemplate->setPartialRootPath($partialRootPath);
+		}
+
+		$layoutRootPath = $this->tsValue('layoutRootPath');
+		if ($layoutRootPath !== NULL) {
+			$fluidTemplate->setLayoutRootPath($layoutRootPath);
+		}
+
+			// Template resources need to be evaluated from the templates package not the requests package.
+		if (strpos($templatePath, 'resource://') === 0) {
+			$templateResourcePathParts = parse_url($templatePath);
+			$fluidTemplate->setResourcePackage($templateResourcePathParts['host']);
+		}
+
+		foreach ($this->variables as $key => $value) {
+			$evaluatedValue = $this->tsRuntime->evaluateProcessor($key, $this, $value);
+			$fluidTemplate->assign($key, $evaluatedValue);
+		}
+			// Assign news to template
+		$fluidTemplate->assign('news', $this->getNewsFromCurrentRequest());
+
+			// TODO this should be done differently lateron
+		$fluidTemplate->assign('fluidTemplateTsObject', $this);
+
+		$sectionName = $this->tsValue('sectionName');
+
+		if ($sectionName !== NULL) {
+			return $fluidTemplate->renderSection($sectionName);
+		} else {
+			return $fluidTemplate->render();
+		}
+	}
+
+	/**
+	 * Checks for the news in the current plugin request
+	 *
+	 * @return \TYPO3\Flow\Persistence\QueryResultInterface The query result
+	 */
+	public function getNewsFromCurrentRequest() {
+		$pluginArguments = $this->tsRuntime->getControllerContext()->getRequest()->getPluginArguments();
+		if (!empty($pluginArguments)) {
+			foreach ($pluginArguments as $arguments) {
+				if ($arguments['@package'] === 'Lelesys.Plugin.News'
+						&& $arguments['@controller'] === 'News'
+						&& $arguments['@action'] === 'show'
+						&& !empty($arguments['news']['__identity'])) {
+					return $this->newsService->findById($arguments['news']['__identity']);
+				}
+			}
+		}
+	}
+
+}
+
+?>
